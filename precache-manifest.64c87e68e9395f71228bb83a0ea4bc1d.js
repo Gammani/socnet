84d14c3fad311042eff10901e6e4d09ac3f1e12a
Oreda-Z <shtucer31@gmail.com>
2020-08-30 18:10:37 +0300

    Updates

---
+++ precache-manifest.64c87e68e9395f71228bb83a0ea4bc1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8461bd25869baec70ecee77d4d71e985",
+    "url": "/socnet/index.html"
+  },
+  {
+    "revision": "b9c4b97c9fdd091efbd3",
+    "url": "/socnet/static/css/main.928fe6fc.chunk.css"
+  },
+  {
+    "revision": "062dd61efa167b3e1f06",
+    "url": "/socnet/static/js/2.965dadef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socnet/static/js/2.965dadef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c4b97c9fdd091efbd3",
+    "url": "/socnet/static/js/main.389c0e68.chunk.js"
+  },
+  {
+    "revision": "e02a0bf2533823ecef6f",
+    "url": "/socnet/static/js/runtime-main.63c415cb.js"
+  }
+]);
