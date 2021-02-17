c5f6f2ef3850b7182368c99ff4ea4ef477b31f26
Oreda-Z <shtucer31@gmail.com>
2021-02-17 19:01:56 +0300

    lesson 41 (addPost, reducer)

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,20 @@ import store from "./redux/store";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-let rerenderEntireTree = () => {
+let _callSubscriber = () => {
     ReactDOM.render(<App state={store.getState()}
                          dispatch={store.dispatch.bind(store)}
+                         store={store}
 
     />, document.getElementById('root'));
 }
 
-store.subscribe(rerenderEntireTree);
-store._rerenderEntireTree();
+store.subscribe(_callSubscriber);
+_callSubscriber();
+
+
+// store.subscribe(rerenderEntireTree);
+// store._rerenderEntireTree();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
